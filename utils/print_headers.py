320b4694b3d6da7e9e95d27f2734afc8d9d107a8
ricda-si <ric.ssr@proton.me>
2025-09-19 21:46:53 +0100

    .

--- utils/print_headers.py
+++ utils/print_headers.py
@@ -3,5 +3,5 @@ import os
 def print_header(title):
     size = len(title)
     print("+" + ("-" * size) + "+")
-    print(f"|{title.capitalize()}|")
+    print(f"| -> {title.capitalize()} <- |")
     print("+" + ("-" * size) + "+")
