9fc96c463df82c1a0c94ff618db21336a0684c98
ricda-si <ric.ssr@proton.me>
2025-09-20 01:09:50 +0100

    .

--- tools/nmap_scan.py
+++ tools/nmap_scan.py
@@ -10,32 +10,49 @@ class NmapScanner:
         self.save_path = save_path
 
     def menu(self):
-        system("clear")
-        print_header("Nmap Scan")
-        nmap_op_print()
-        user = input("> ")
-        match user:
-            case '1':
-                self.simple_scan()
-            case '2':
-                self.advanced_scan()
-            case '3':
-                flags = input("Custom Scan flags -> ")
-                self.custom_scan(flags)
+        while True:
+            system("clear")
+            print_header("Nmap Scan")
+            nmap_op_print()
+            user = input("> ")
+            match user:
+                case '1':
+                    self.simple_scan()
+                case '2':
+                    self.advanced_scan()
+                case '3':
+                    flags = input("Custom Scan flags -> ")
+                    self.custom_scan(flags)
+                case '99':
+                    break
 
     def simple_scan(self):
+        print(f"[+] Running 'nmap {self.target} -p- -Pn -T4'")
         cmd = ["nmap", self.target, "-p-", "-Pn", "-T4"]
         result = subprocess.run(cmd, capture_output=True, text=True)
         output = result.stdout
         open_ports = re.findall(r"(\d+)/tcp\s+open", output)
         if open_ports:
-            print(open_ports)
+            for port in open_ports:
+                print(f"Port: {port}")
             input()
         else:
             print("No open ports.\n")
             input()
 
     def advanced_scan(self, ports='-'):
+        print(f"[+] Running 'nmap {self.target} -A -T4 -p{ports} -Pn -T4'")
+        cmd = ["nmap", self.target, f"-p{ports}", "-Pn", "-T4", "-A"]
+        result = subprocess.run(cmd, capture_output=True, text=True)
+        output = result.stdout
+        open_ports = re.findall(r"(\d+)/tcp\s+open", output)
+        if open_ports:
+            for port in open_ports:
+                print(f"Port: {port}")
+            input()
+        else:
+            print("No open ports.\n")
+            input()
         system(f"nmap -A -T4 -p{ports} -Pn {self.target} -oN {self.save_path}/advanced_scan.txt")
 
     def custom_scan(self, flags):
